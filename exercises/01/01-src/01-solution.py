f0a0acb37cfa6c60fc13c2c4c688fc6f6be80109
Alberto <albertopga@gmail.com>
2020-07-27 18:16:21 +0200

    Ejercicio 1 completado

--- exercises/01/01-src/01-solution.py
+++ exercises/01/01-src/01-solution.py
@@ -1,1 +1,8 @@
-print('Your solution should be there')
+from datetime import datetime
+
+txt1 = "Today is:"
+txt2 = "Have a nice day."
+
+formateddate = datetime.strftime(datetime.now(), '%H:%M:%S - %d/%m/%y')
+
+print(txt2,txt1,formateddate)
