94ab81067f4c939851be9151084b372c0beefaac
Alberto <albertopga@gmail.com>
2020-07-31 12:17:25 +0200

    exercice3

--- challenges/01/01-src/01-solution.py
+++ challenges/01/01-src/01-solution.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 txt1 = "Today is:"
 txt2 = "Have a nice day."
+strFormat = "%H:%M:%S - %d/%m/%y"
 
-formatedDate = datetime.strftime(datetime.now(), '%H:%M:%S - %d/%m/%y')
+formatedDate = datetime.strftime(datetime.now(), strFormat)
 
-print(txt2+" "+txt1+" "+formatedDate)
-print(txt2,txt1,formatedDate)
+print(txt2 + " " + txt1 + " " + formatedDate)
+print(txt2, txt1, formatedDate)
 
--- challenges/02/02-src/02-solution.py
+++ challenges/02/02-src/02-solution.py
@@ -1,13 +1,13 @@
 from functools import reduce
 
-number = int(input("Insert a integer number"))
+number = int(input("Insert a integer number "))
 result1 = 0
 result2 = 0
 
-for n in range(0,number+1):
+for n in range(0, number + 1):
     result1 += n
 
-for n in reversed(range(0,number+1,2)):
+for n in reversed(range(0, number +1, 2)):
     result2 += n
 
-print("The difference is",result1-result2)
+print("The difference is ", result1 - result2)
